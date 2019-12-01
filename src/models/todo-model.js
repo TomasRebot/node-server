96f7fde7f8a4db201d963f4cc6dad5f9cce3a8d5
Tomas rebot <tomas.devel@gmail.com>
2019-12-01 15:05:34 -0300

    added date type for todo model, status for todo model and required for owner key in todo model

--- src/models/todo-model.js
+++ src/models/todo-model.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//date format is yyyy-mm-dd
 const todoSchema = new Schema({
     title: String,
     description: String,
-    date: String,
+    date: Date,
+    status: {
+        type: String,
+        enum: ['finished','pending'],
+        default: 'pending'
+    },
     owner: {
         type: Schema.Types.ObjectId,
         ref: 'user'
