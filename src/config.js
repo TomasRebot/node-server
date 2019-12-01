0f031d672f7cdb7c87432dfbb67b77306f390039
Tomas rebot <tomas.devel@gmail.com>
2019-12-01 03:08:02 -0300

    hot fix for deploy

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const dbProduction = "mongodb+srv://tomi:homar666@cluster0-9fmpn.mongodb.net/test?retryWrites=true&w=majority";
 const dbLocal = "mongodb://localhost/mcga";
-const environment = "local";
+const environment = "production";
 const port = process.env.PORT || 3000;
 const secretKey = process.env.SECRET_TOKEN || 'mcga-todo-application';
 
 const getDB =  () => {
-    if(environment === 'production'){
+    if(environment === 'local'){
         return dbLocal;
     }else{
         return dbProduction;
